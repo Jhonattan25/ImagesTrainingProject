1cca901f60a53ab70d3eb7c9b25845170bd38b4e
Jhonattan Orjuela <jhonattan1997@hotmail.com>
2021-11-23 09:49:51 -0500

    Functions were created to take the image reference and save it to the server

--- app/app.js
+++ app/app.js
@@ -2,8 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const bearerToken = require('express-bearer-token');
-const cookieParser = require('cookie-parser');
+const fileUpload = require('express-fileupload');
 
 const images = require('./routes/images'); 
 
@@ -11,8 +10,7 @@ const app = express()
   .use(cors({credentials: true, origin: 'http://localhost:4200'}))
   .use(bodyParser.json())
   .use(bodyParser.urlencoded({extended: true}))
-  .use(cookieParser())
-  .use(bearerToken());
+  .use(fileUpload());
 
 app.use('/images', images);
 
--- app/controllers/images-controller.js
+++ app/controllers/images-controller.js
@@ -10,6 +10,27 @@ let consultImages = (req, res) => {
     });
 };
 
+let uploadImage = (req, res) => {
+  let documentReportId = req.body.id;
+  let url = "defaultDocumentReport.png";
+
+  if (req["files"]) {
+    url = `${documentReportId}DocumentReport.png`;
+    req["files"].files.mv(`./images/${url}`, function (err) {
+      if (err) console.log(err);
+    });
+  }
+
+  db.uploadImages({ documentReportId, url })
+    .then((result) => {
+      return res.status(201).json({ Status: "Upload ok", up: true });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
 module.exports = {
   consultImages,
+  uploadImage,
 };
